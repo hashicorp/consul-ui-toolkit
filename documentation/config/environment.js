4a5da5cbb5c169807be18e1f11e0591ca951f9e3
wenincode <tyler.wendlandt@hashicorp.com>
2023-03-01 08:45:07 -0700

    Rename test-app to documentation

--- documentation/config/environment.js
+++ documentation/config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function (environment) {
   const ENV = {
-    modulePrefix: 'test-app',
+    modulePrefix: 'documentation',
     environment,
     rootURL: '/',
     locationType: 'history',
