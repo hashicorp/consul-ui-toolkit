d44b7bc9be2d96cf3b9916b81842b256317aabfa
valeriia-ruban <valeriia.ruban@hashicorp.com>
2023-03-27 13:25:38 -0700

    added ember/types

--- documentation/types/global.d.ts
+++ documentation/types/global.d.ts
@@ -2,11 +2,10 @@
  * Copyright (c) HashiCorp, Inc.
  */
 
-import { ComponentLike, HelperLike } from '@glint/template';
+import { HelperLike } from '@glint/template';
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry {
-    WelcomePage: ComponentLike;
     'page-title': HelperLike<{
       Args: { Positional: [title: string] };
       Return: void;
