9241266ccbf578d8100657cf74ae6f539583d7f9
wenincode <tyler.wendlandt@hashicorp.com>
2023-04-14 15:13:29 -0600

    Update style import

--- documentation/ember-cli-build.js
+++ documentation/ember-cli-build.js
@@ -23,6 +23,7 @@ module.exports = function (defaults) {
       precision: 4,
       includePaths: [
         '../node_modules/@hashicorp/design-system-tokens/dist/products/css',
+        '../node_modules/@hashicorp/design-system-components/app/styles',
       ],
     },
     minifyCSS: {
