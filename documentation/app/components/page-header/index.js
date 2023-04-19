95acf7f8c8d0acbb1657afa54beeb0e1cfdfe7db
wenincode <tyler.wendlandt@hashicorp.com>
2023-04-19 11:55:19 -0600

    Update css to use more hds tokens

--- documentation/app/components/page-header/index.js
+++ documentation/app/components/page-header/index.js
@@ -4,20 +4,6 @@
 
 import Component from '@glimmer/component';
 
-// enum COMPONENT_STATUS {
-//   DEVELOPMENT = 'development',
-//   COMPLETE = 'complete',
-// }
-
-// export interface ComponentSignature {
-//   Args: {
-//     title?: string;
-//     figma?: string;
-//     github?: string;
-//     status?: COMPONENT_STATUS;
-//   };
-// }
-
 const STATUS_BADGES = {
   development: {
     text: 'Dev In Progress',
