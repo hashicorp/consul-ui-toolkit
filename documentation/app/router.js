664f40d95e1b681fc5a47b24bcb250e53c7149b7
Tyler Wendlandt <tyler.wendlandt@hashicorp.com>
2023-05-16 10:55:52 -0600

    CC-4850: in page nav for docs (#20)
    * feat: add initial in-page-nav component
    
    * fix: remove margin on code-blocks
    
    * feat: get hash links to work and update demo to use yielded component
    
    * feat: update in-page-nav styling to use tokens
    
    * feat: add example that is below the fold
    
    * chore: add copywrite headers
    
    * feat: update in-page-nav link class and styling
    
    * chore: add test for in-page-nav
    
    * chore: add copywrite headers
    
    * feat: make the in-page-nav stick
    
    * fix: add ids to headings on test list-item page

--- documentation/app/router.js
+++ documentation/app/router.js
@@ -4,7 +4,9 @@
 
 import EmberRouter from '@ember/routing/router';
 import config from 'documentation/config/environment';
+import { withHashSupport } from 'ember-url-hash-polyfill';
 
+@withHashSupport
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
---
+++ documentation/tests/integration/components/in-page-nav-test.js
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ */
+
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { findAll, render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | InPageNav', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders the links', async function (assert) {
+    await render(hbs`
+      <InPageNav as |I|>
+        <I.Link @section="#test-one" @depth={{1}}>Test Link 1</I.Link>
+        <I.Link @section="#test-two" @depth={{2}}>Test Link 2</I.Link>
+        <I.Link @depth={{1}}>Link with no section</I.Link>
+        <I.Link @section="#test-three">Link with no depth</I.Link>
+      </InPageNav>,
+    `);
+
+    const links = findAll('.doc-InPageNav-link');
+
+    assert.strictEqual(links.length, 4, 'There should be 4 links on the page');
+    assert
+      .dom('.doc-InPageNav-link:nth-child(1)')
+      .hasClass(
+        'doc-InPageNav-link--depth-1',
+        'There should be the level 1 depth class'
+      );
+
+    assert
+      .dom('.doc-InPageNav-link:nth-child(1) > a')
+      .hasAttribute(
+        'href',
+        '#test-one',
+        'Should have the #test-one href value'
+      );
+
+    assert
+      .dom('.doc-InPageNav-link:nth-child(2)')
+      .hasClass(
+        'doc-InPageNav-link--depth-2',
+        'There should be the level 2 depth class'
+      );
+
+    assert
+      .dom('.doc-InPageNav-link:nth-child(3) > a')
+      .hasAttribute(
+        'href',
+        '#',
+        "It should have a '#' href value when @section is not passed in"
+      );
+
+    assert
+      .dom('.doc-InPageNav-link:nth-child(4)')
+      .hasClass(
+        'doc-InPageNav-link--depth-1',
+        'There should be the level 1 depth class when depth is not passed in'
+      );
+  });
+});
