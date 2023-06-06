f07c65e43c0ab3328a3e626a11ae242298aed28e
valeriia-ruban <valeriia.ruban@hashicorp.com>
2023-06-05 22:21:59 -0700

    add license

--- documentation/tests/pages/service-instance-list-item.js
+++ documentation/tests/pages/service-instance-list-item.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 import { create, isPresent, isVisible, text } from 'ember-cli-page-object';
 
 const listItemSelector = '[data-test-service-instance-list-item]';
