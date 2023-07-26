f6055a0b77472edb0d1657f2eccc4927a7772a0f
wenincode <tyler.wendlandt@hashicorp.com>
2023-07-26 16:47:39 -0600

    [DRAFT] state, checkbox, checkmarks working

--- toolkit/src/components/cut/filter-bar/filter/checkmark.ts
+++ toolkit/src/components/cut/filter-bar/filter/checkmark.ts
@@ -9,12 +9,11 @@ interface FilterBarCheckbox {
     name: string;
     value: any;
     toggle: (name: string, value: any) => void;
-    softToggle: (name: string, displayName: string, value: any) => void;
     isChecked: (name: string, value: any) => boolean;
   };
 }
 
-export default class CheckboxComponent extends Component<FilterBarCheckbox> {
+export default class CheckmarkComponent extends Component<FilterBarCheckbox> {
   get isChecked() {
     return this.args.isChecked(this.args.name, this.args.value);
   }
--- toolkit/src/components/cut/filter-bar/index.ts
+++ toolkit/src/components/cut/filter-bar/index.ts
@@ -9,117 +9,217 @@ import { tracked } from '@glimmer/tracking';
 
 export interface ComponentSignature {
   Args: {
-    href?: string;
-    isHrefExternal?: boolean;
-    route?: string;
-    isRouteExternal?: boolean;
-    query?: object;
-    replace?: string;
-    onClick(): void;
+    config: FilterConfig;
+    onChange: (config: FilterConfig) => void;
   };
 }
 
-interface Filter {
+export interface FilterConfig {
+  search?: Search;
+  filters?: Filters;
+  sort?: { value?: string };
+}
+
+interface Search {
+  value?: string;
+}
+
+export interface Filter {
   text: string;
   value: any;
 }
 
-interface Filters {
+export interface Filters {
   [name: string]: Filter;
 }
 
+// 'clusterID'.replace(/([A-Z]+)/g, " $1").replace(/([A-Z][a-z])/g, " $1").replace(/  /g, " ").replace(/^./g, (match) => match.toUpperCase())
 export default class FilterBarComponent extends Component<ComponentSignature> {
-  @tracked filters: Filters = {
-    status: { value: ['running'], text: 'Status' },
-  };
-  @tracked filterChanges: Filters = {};
+  @tracked configChanges: FilterConfig = {};
+  // @tracked localConfig?: FilterConfig;
+  titlecase(value: string): string | null {
+    if (!value || typeof value !== 'string') {
+      return null;
+    }
 
+    return value
+      .replace(/([A-Z]+)/g, ' $1')
+      .replace(/([A-Z][a-z])/g, ' $1')
+      .replace(/  /g, ' ')
+      .replace(/^./g, (match) => match.toUpperCase());
+  }
+
+  get localConfig(): FilterConfig {
+    /*
+      - take config
+      - if there are no filter changes return config
+      - if there are filter changes apply them to config and return them
+    */
+    let local = Object.assign({}, this.args.config);
+
+    if (this.configChanges.filters) {
+      // do the filter changes
+      local.filters = Object.assign(
+        {},
+        local.filters,
+        this.configChanges.filters
+      );
+    }
+
+    if (this.configChanges.search) {
+      // do the search changes
+      local.search = Object.assign({}, local.search, this.configChanges.search);
+    }
+
+    if (this.configChanges.sort) {
+      // do the sort changes
+      local.sort = Object.assign({}, local.sort, this.configChanges.sort);
+    }
+
+    return local;
+  }
+
+  @action
   isCheckboxChecked(filter: string, value: any) {
-    console.log(this.filters);
-    console.log(this.filters[filter], filter, value);
-
-    return this.filters[filter].value.includes(value);
-  }
-
-  @action
-  softToggleFilterValue(filterName: string, value: Filter) {
-    // grab existing filter, existing filter changes
-    // toggle filter value on the above
-    // apply filter changes to the filterchanges tracked attribute
-    console.log('BEFORE:');
-    console.log('Filter Changes:', this.filterChanges);
-
-    let filter: Filters = Object.assign(
-      {},
-      { [filterName]: this.filters[filterName] }
-    );
-
-    if (this.filterChanges[filterName]) {
-      filter = Object.assign(filter, {
-        [filterName]: this.filterChanges[filterName],
-      });
-    }
-
-    if (Array.isArray(this.filters[filterName])) {
-      const valueIndex = filter[filterName].value.indexOf(value);
-
-      if (valueIndex !== -1) {
-        filter[filterName].value.splice(valueIndex, 1);
-      } else {
-        filter[filterName].value.push(value);
-      }
-    }
-
-    const newFilterChanges = Object.assign({}, this.filterChanges, filter);
-    this.filterChanges = newFilterChanges;
-    console.log('AFTER:');
-    console.log('Filter Changes:', this.filterChanges);
-  }
-
-  @action
-  toggleFilterValue(filterName: string, value: Filter) {
-    // grab existing filter, existing filter changes
-    // toggle filter value on the above
-    // apply filter changes to the filterchanges tracked attribute
-    let filter: Filters = Object.assign(
-      {},
-      { [filterName]: this.filters[filterName] }
-    );
-
-    if (this.filterChanges[filterName]) {
-      filter = Object.assign(filter, {
-        [filterName]: this.filterChanges[filterName],
-      });
-    }
-
-    if (Array.isArray(this.filters[filterName])) {
-      const valueIndex = filter[filterName].value.indexOf(value);
-
-      if (valueIndex !== -1) {
-        filter[filterName].value.splice(valueIndex, 1);
-      } else {
-        filter[filterName].value.push(value);
-      }
-    } else if (filter[filterName] === value) {
-      // TODO: fix this
-      // filter[filterName] = undefined;
+    if (Array.isArray(this.localConfig?.filters?.[filter]?.value)) {
+      return this.localConfig?.filters?.[filter]?.value?.includes(value);
     } else {
-      filter[filterName] = value;
+      return this.localConfig?.filters?.[filter]?.value === value;
+    }
+  }
+
+  /**
+   *
+   * @param name
+   * @param displayName
+   * @param value
+   * @param isMultiSelect
+   *
+   * softToggleFilterValue extracts the filter that is being changed from the passed in config,
+   * applies the existing pending config changes for that filter to it, then applies the new
+   * change to it and saves it back to the configChanges object so that it may be applied in
+   * the future.
+   *
+   * TODO:
+   *  this works best with arrays of basic data types and basic data types best right now.
+   *  Need to find a way to identify custom objects so that we can compare them.
+   *  This also doesn't support passing in display copy for value types currently. By that
+   *  I mean if you have a value that isn't easily or nicely converted to a string to show in
+   *  the applied filters section of the filter bar, it would be nice to be able to pass in copy
+   *  for how it should be represented there.
+   */
+  @action
+  softToggleFilterValue(
+    name: string,
+    displayName: string,
+    value: any,
+    isMultiSelect?: boolean
+  ) {
+    console.log('BEFORE:', this.configChanges);
+    debugger;
+    let filterChange: Filters = {
+      [name]: { text: displayName, value: undefined },
+    };
+
+    if (this.localConfig?.filters?.[name]) {
+      filterChange = Object.assign(filterChange, {
+        [name]: this.localConfig?.filters?.[name],
+      });
     }
 
-    this.filters = Object.assign({}, this.filters, {
-      [filterName]: filter[filterName],
-    });
+    if (this.configChanges?.filters?.[name]) {
+      filterChange = Object.assign(filterChange, {
+        [name]: this.configChanges?.filters?.[name],
+      });
+    }
+
+    if (isMultiSelect) {
+      if (Array.isArray(filterChange[name].value)) {
+        let valueIndex = filterChange[name].value?.indexOf(value);
+
+        if (valueIndex !== -1) {
+          filterChange[name].value.splice(valueIndex, 1);
+        } else {
+          filterChange[name].value.push(value);
+        }
+      } else {
+        filterChange[name].value = [value];
+      }
+    } else if (typeof value === 'object') {
+      filterChange[name].value = value;
+    } else {
+      if (filterChange[name].value === value) {
+        filterChange[name].value = undefined;
+      } else {
+        filterChange[name].value = value;
+      }
+    }
+
+    this.configChanges.filters = Object.assign(
+      this.configChanges?.filters || {},
+      filterChange
+    );
+
+    console.log('AFTER:', this.configChanges);
+    this.applyFilter(name);
+  }
+
+  /**
+   *
+   * @param filterName
+   * @param displayName
+   * @param value
+   * @param isMultiSelect
+   *
+   * toggleFilterValue toggles filters and applies the change immediately. This will call the softToggleValue
+   * function, followed by the applyFilter function to perform this.
+   */
+  @action
+  toggleFilterValue(
+    filterName: string,
+    displayName: string,
+    value: Filter,
+    isMultiSelect?: boolean
+  ) {
+    this.softToggleFilterValue(filterName, displayName, value, isMultiSelect);
+    this.applyFilter(filterName);
   }
 
   // apply filterChanges to filters
   // delete that filters change in the filterchanges object
+  /**
+   *
+   * @param name
+   */
   @action
   applyFilter(name: string) {
-    this.filters = Object.assign({}, this.filters, {
-      [name]: this.filterChanges[name],
-    });
+    // grab the config
+    let config = Object.assign({}, this.args.config);
 
-    console.log(this.filters);
+    // apply the filter from filterChanges
+    if (this.configChanges?.filters?.[name]) {
+      config.filters = Object.assign({}, config.filters, {
+        [name]: this.configChanges?.filters?.[name],
+      });
+    }
+
+    // clear out the empty filters
+    if (config.filters) {
+      Object.keys(config.filters).forEach((key) => {
+        if (
+          !config?.filters?.[key]?.value ||
+          (Array.isArray(config?.filters?.[key]?.value) &&
+            config?.filters?.[key]?.value.length === 0)
+        ) {
+          delete config.filters?.[key];
+        }
+      });
+    }
+
+    // call it
+    this.args.onChange(config);
+
+    // clear out the config changes for that filter
+    delete this.configChanges?.filters?.[name];
   }
 }
