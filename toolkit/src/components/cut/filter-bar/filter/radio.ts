f68fa5444614976284aa856f8f3b315a25d29962
wenincode <tyler.wendlandt@hashicorp.com>
2023-07-31 08:57:49 -0600

    Add radio filter

---
+++ toolkit/src/components/cut/filter-bar/filter/radio.ts
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ */
+
+import Component from '@glimmer/component';
+
+interface FilterBarCheckbox {
+  Args: {
+    filterName: string;
+    name: string;
+    value: any;
+    toggle: (name: string, value: any) => void;
+    isChecked: (name: string, value: any) => boolean;
+  };
+}
+
+export default class CheckmarkComponent extends Component<FilterBarCheckbox> {
+  get isChecked() {
+    return this.args.isChecked(this.args.filterName, this.args.value);
+  }
+}
