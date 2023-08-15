482438f0d4e49c1679f861d1b84c7994cb659050
wenincode <tyler.wendlandt@hashicorp.com>
2023-08-14 21:56:03 -0600

    Cleanup and add comments

--- toolkit/src/components/cut/list/pagination.ts
+++ toolkit/src/components/cut/list/pagination.ts
@@ -7,27 +7,51 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import RouterService from '@ember/routing/router-service';
 
-/**
- * <List as |L|>
- *  <L.FilterBar as |FB|>
- *  </L.FilterBar>
- *
- *  <L.
- * </List>
- */
-
 interface PaginationArgs {
   Args: {
     nextCursor?: string;
     prevCursor?: string;
-    model?: unknown;
+    pageSizes?: number[];
+    currentPageSize?: number;
     queryFunction?: (page: string) => {
       [key: string]: string | number | unknown;
     };
+    onPageChange?: (page: string) => void;
     onPageSizeChange?: (size: number) => void;
   };
 }
 
+/**
+ * `Cut::List::Pagination` provides a wrapper around the `Hds::Pagination::Compact` and `Hds::Pagination::SizeSelector` components.
+ *
+ * To use with route-based pagination
+ * ```
+ * <L.Pagination
+ *  @pageSizes={{array 5 10 20 30}}
+ *  @currentPageSize={{this.size}}
+ *  @prevCursor={{this.prevCursor}}
+ *  @nextCursor={{this.nextCursor}}
+ * />
+ * ```
+ *
+ * To use with functions
+ * ```
+ * <L.Pagination
+ *  @pageSizes={{array 5 10 20 30}}
+ *  @currentPageSize={{this.size}}
+ *  @prevCursor={{this.prevCursor}}
+ *  @nextCursor={{this.nextCursor}}
+ *  @onPageChange={{this.updatePage}}
+ *  @onPageSizeChange={{this.updatePageSize}}
+ * />
+ * ```
+ *
+ * @class PaginationComponent
+ *
+ * By default the pagination will use the router service to update the existing query params with the `page` and `pageSize` parameters
+ * when interacting with the page selectors and the size selectors. You can pass in a `onPageChange` action to have it call a function insteand.
+ * The same goes for the size selector, but the argument is `onPageSizeChange`.
+ */
 export default class PaginationComponent extends Component<PaginationArgs> {
   @service declare readonly router: RouterService;
 
