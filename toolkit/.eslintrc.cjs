080c27bc6f7077ed37a582c613244df230a418b0
wenincode <tyler.wendlandt@hashicorp.com>
2023-12-21 12:53:27 -0700

    Upgrade eslint and prettier packages

--- toolkit/.eslintrc.cjs
+++ toolkit/.eslintrc.cjs
@@ -31,6 +31,7 @@ module.exports = {
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-duplicate-enum-values': 'off'
   },
   overrides: [
     // node files
