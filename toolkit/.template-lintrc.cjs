cbfacb5bb119d2d6daa64d49e7d953a5ea00927a
wenincode <tyler.wendlandt@hashicorp.com>
2023-12-22 09:51:20 -0700

    Fix or ignore template lint errors

--- toolkit/.template-lintrc.cjs
+++ toolkit/.template-lintrc.cjs
@@ -6,4 +6,19 @@
 
 module.exports = {
   extends: 'recommended',
+  overrides: [
+    {
+      files: '**/components/cut/filter-bar/filter/filter-input.hbs',
+      rules: {
+        'no-inline-styles': false
+      }
+    },
+    // TODO: investigate which splattributes needs to remain
+    {
+      files: '**/components/cut/list-item/index.hbs',
+      rules: {
+        'no-nested-splattributes': false
+      }
+    }
+  ]
 };
